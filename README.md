64254a0ed9b07dddc972344ecc2a3e0dd227f020
johannesstroebele91 <stroebele.j@gmail.com>
2024-04-16 22:34:33 +0200

    Add mandatory registration option for user

---
+++ README.md
@@ -0,0 +1,24 @@
+# Tyto: Pomodoro Productivity Tracker
+
+Tyto is a cloud-based application designed to boost your productivity using the Pomodoro Technique. It allows you to set focused 25-minute work intervals, helping you achieve more throughout your day. This project utilizes TypeScript, Angular, Angular Material, Firebase Authentication, and Vercel for a smooth, performant, and cross-platform user experience.
+
+# Features
+
+- 25-Minute Pomodoro Timer: Set and track your work intervals with a clean and intuitive interface.
+- Start/Stop and Reset Buttons: Easily control your timer and start fresh whenever needed.
+- Login and Registration: Securely register and log in using your email and password.
+
+# Technology
+
+- Mobile-First Design: for seamless use across various platforms.
+- Frontend: built with Angular and Angular Material for a robust and up-to-date development foundation.
+- Routing with Authentication Guards: Restricts access based on user login status.
+- Authentication: Secure user access through Firebase's built-in authentication features.
+- Data Storing: Retrieves and stores data efficiently using Firebase's services.
+- Vercel Deployment: Leverages Vercel's serverless platform for scalability and reliability.
+
+# Development and Deployment
+
+- Development Server: Run ng serve for a local development server accessible at http://localhost:4040/. Changes to source files trigger automatic reloading.
+- Build: Run ng build to create production-ready build artifacts stored in the dist/ directory. Use the --prod flag for optimized production builds.
+- Production Deployment: After building, run node server.js to start the production server. Deployment can be automated using Vercel's continuous integration features.
---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {MatNativeDateModule} from "@angular/material/core";
+import {Router, RouterLink, RouterOutlet} from "@angular/router";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatCalendar} from "@angular/material/datepicker";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatToolbar, MatToolbarModule} from "@angular/material/toolbar";
+import {NgIf} from "@angular/common";
+import {Subscription} from "rxjs";
+import {AuthService} from "./services/auth.service";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [MatToolbarModule, MatNativeDateModule, RouterOutlet, MatButton, MatCalendar, MatIcon, MatMenuTrigger, MatIconButton, MatMenu, MatCard, MatCardTitle, MatCardHeader, MatCardContent, MatToolbar, RouterLink, NgIf],
+  template: `
+    <mat-toolbar color="primary">
+      <mat-toolbar-row>
+        <span style="letter-spacing: 1px">{{ title }}</span>
+        <mat-icon style="margin-left: 5px">alarm</mat-icon>
+        <span style="flex: 1 1 auto;"></span>
+        <a *ngIf="isNotOnLoginOrRegisterPage() && isAuthenticated" routerLink="/" (click)="logout()"
+           style="color: white; text-decoration: none; font-size: 16px">Logout</a>
+      </mat-toolbar-row>
+    </mat-toolbar>
+    <div style=" margin: 60px auto;">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+})
+export class AppComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  title = 'Tyto';
+  private userSub: Subscription | undefined;
+
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.autoLoginAfterReload();
+
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isAuthenticated = !user ? false : true;
+    })
+  }
+
+  isNotOnLoginOrRegisterPage(): boolean {
+    const isOnLoginPage = this.router.url !== '/';
+    const isOnRegisterPage = this.router.url !== '/register';
+    return (isOnLoginPage && isOnRegisterPage);
+  }
+
+  logout() {
+    this.authService.logout()
+  }
+
+  ngOnDestroy(): void {
+    this.userSub?.unsubscribe()
+  }
+}
